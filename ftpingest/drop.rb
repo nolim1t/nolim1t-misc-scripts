f977bc5a999d2d83b533fa9a2ba53baca29cda70
Barry (@nolim1t) <nolimit@perceptionz.net>
2011-08-26 16:53:04 +1000

    Add in FTP Ingest script

---
+++ ftpingest/drop.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+server = ENV['ftpserver']
+user = ENV['user']
+pass = ENV['pass']
+file = ENV['file']
+
+require 'net/ftp'
+require 'guid'
+
+if server != nil then
+    ftp = Net::FTP.new(server)
+    ftp.login(user, pass)
+    thefilename = "#{Guid.new.to_s}.mp4}"
+    puts "Sending #{file} as #{thefilename}"
+    ftp.putbinaryfile(file,thefilename)
+    ftp.close
+else
+  puts "Make sure you export the following variables - ftpserver, user, pass, and file"
+end
