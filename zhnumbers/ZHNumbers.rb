95d9d6008e469520aae2b3669983dfb90d6fde4e
Barry <barry.teoh@perceptionz.net>
2014-11-12 13:54:52 +1100

    make it a functin. Add a zero

--- zhnumbers/ZHNumbers.rb
+++ zhnumbers/ZHNumbers.rb
@@ -1,20 +1,26 @@
 # Convert number to chinese
-numbers = {
-    '1' => '一',
-    '2' => '二',
-    '3' => '三',
-    '4' => '四',
-    '5' => '五',
-    '6' => '六',
-    '7' => '七',
-    '8' => '八',
-    '9' => '十'
-}
-ts = (Time.now.to_f * 1000).to_i.to_s
-l = ts.length
-zh = ''
-l.times do |i|
-  idx = ts[i,i+1][0].to_i
-  zh = zh + numbers["#{idx}"]
+def ntz(the_num)
+  numbers = {
+      '0' => '零',
+      '1' => '一',
+      '2' => '二',
+      '3' => '三',
+      '4' => '四',
+      '5' => '五',
+      '6' => '六',
+      '7' => '七',
+      '8' => '八',
+      '9' => '十'
+  }
+  the_num_str = the_num.to_s
+  l = the_num_str.length
+  zh = ''
+  l.times do |i|
+    idx = the_num_str[i,i+1][0].to_i
+    to_append = numbers["#{idx}"]
+    zh = "#{zh}#{to_append}"
+  end
+  return zh
 end
-puts zh
+
+puts ntz((Time.now.to_f * 1000).to_i)
